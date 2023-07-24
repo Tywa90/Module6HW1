8752e8c47006db73d24322102157b43c15744768
Serhii Amelin <amserega90@gmail.com>
2023-07-24 16:20:00 +0300

    Add new controller

---
+++ Catalog/Catalog.Host/Controllers/NotebookItemController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Host.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class NotebookItemController : ControllerBase
+{
+    private static readonly string[] Brands = new[]
+    {
+        "Acer", "ASUS", "HP", "Lenovo", "Apple", "DELL"
+    };
+
+    private static readonly string[] ProcessorsTypes = new[]
+    {
+        "Intel Core i3", "Intel Core i5", "Intel Core i7", "AMD Ryzen 3", "AMD Ryzen 5", "AMD Ryzen 7"
+    };
+
+    private static readonly string[] GraphicsCardType = new[]
+    {
+        "integrated", "GeForce RTX 3080", "GeForce RTX 3060", "GeForce GTX 1650", "GeForce GTX 1650", "GeForce 4060"
+    };
+
+    private readonly ILogger<NotebookItemController> _logger;
+
+    public NotebookItemController(ILogger<NotebookItemController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<NotebookItem> Get()
+    {
+        return Enumerable.Range(1, Brands.Length).Select(index => new NotebookItem
+        {
+            BrandName = Brands[Random.Shared.Next(Brands.Length)],
+            Processor = ProcessorsTypes[Random.Shared.Next(ProcessorsTypes.Length)],
+            GraphicsCard = GraphicsCardType[Random.Shared.Next(GraphicsCardType.Length)],
+            Price = Math.Round(Random.Shared.NextDouble() * 1000, 2),
+            OrderDate = DateTime.Now.AddDays(Random.Shared.NextDouble() * 10)
+        })
+            .ToArray();
+    }
+}
---
+++ Catalog/Catalog.Host/NotebookItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.Host
+{
+    public class NotebookItem
+    {
+        public string BrandName { get; set; }
+        public string Processor { get; set; }
+        public string GraphicsCard { get; set; }
+        public double Price { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
